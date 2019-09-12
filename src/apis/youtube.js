30e37a16dda1f703656f5128a6e571db479c36ca
Jayger <jaygerzero@gmail.com>
2019-09-12 19:53:14 -0400

    vBrowse added some CSS and API done

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyA1CxbbXKdk5vh_FsyvY17hJIr3FJP-tA0';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (event) => {
         event.preventDefault();
 
-
+        this.props.onFormSubmit(this.state.term)
         //TODO: Make sure we call
         // callback from parent component
     }
